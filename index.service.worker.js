fa27e62142bd5e6656f3e2b3c6810313d85a0946
Casselej <theskitzobros@gmail.com>
2025-05-17 14:12:33 -0300

    hnomxp

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747452648|379053221';
+const CACHE_VERSION = '1747501815|366258169';
 /** @type {string} */
 const CACHE_PREFIX = 'Game I think-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
