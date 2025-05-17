e57c6fb4c10ff49ae66a850c3424440cc2a5e5b3
Casselej <theskitzobros@gmail.com>
2025-05-17 00:31:52 -0300

    Moderately large update
    Death is improved and game now insults you when you die.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747363102|18131225';
+const CACHE_VERSION = '1747452648|379053221';
 /** @type {string} */
 const CACHE_PREFIX = 'Game I think-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
