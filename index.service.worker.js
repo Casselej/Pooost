9812e751e03c51827eabf108bd542accc24d256c
Casselej <theskitzobros@gmail.com>
2025-05-17 21:26:54 -0300

    Static
    Changed every variable to static because performance, I think.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747501815|366258169';
+const CACHE_VERSION = '1747527933|1631532043';
 /** @type {string} */
-const CACHE_PREFIX = 'Game I think-sw-cache-';
+const CACHE_PREFIX = 'Eat stink and di-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
