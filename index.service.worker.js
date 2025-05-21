963b3e4dddca4d1d45036c0c9e4424051d073be2
Casselej <theskitzobros@gmail.com>
2025-05-21 03:38:53 -0300

    ProbablyBrokeb
    Hopefully optimized the game to better work with shitty chromebooks

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747739186|4549449157';
+const CACHE_VERSION = '1747809428|1023555242';
 /** @type {string} */
 const CACHE_PREFIX = 'Eat stink and di-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
