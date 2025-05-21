156698e19597f51ec4829833e8e65af71d13772d
Casselej <theskitzobros@gmail.com>
2025-05-21 04:49:34 -0300

    gazb

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747809428|1023555242';
+const CACHE_VERSION = '1747813763|337518955';
 /** @type {string} */
 const CACHE_PREFIX = 'Eat stink and di-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
