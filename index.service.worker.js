c5b417ce7267e4a61ef725aaa2195639c9052600
Casselej <theskitzobros@gmail.com>
2025-05-23 07:31:14 -0300

    Dropping
    Dropping borken

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1747891783|2350652597';
+const CACHE_VERSION = '1747996229|321687478';
 /** @type {string} */
 const CACHE_PREFIX = 'Eat stink and di-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
